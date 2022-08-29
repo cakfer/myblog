8562e63564feed948972baa23c91737b7f247294
cakfer <112299172+cakfer@users.noreply.github.com>
2022-08-29 17:33:02 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -39,16 +39,18 @@
           <div class="ui-section-content__layout ui-layout-grid ui-layout-grid-2">
             <a href="https://kela.pages.dev/">
             <div class="ui-component-card ui-layout-column-4">
-              <img src="img/preview-twibbon.png" alt="Demo Twibbon" loading="lazy" class="ui-component-card--image">
+              <img src="img/twibbon.png" alt="Demo Twibbon" loading="lazy" class="ui-component-card--image">
               <h4 class="ui-component-card--title">Twibbon Generator.</h4>
               <p>Twibbon generator untuk membuat gambar kampanye media sosial.</p>
             </div>
             </a>
+            <a href="https://kela.pages.dev/qr-code">
             <div class="ui-component-card ui-layout-column-4">
               <img src="img/chart.png" alt="QR Code" loading="lazy" class="ui-component-card--image">
               <h4 class="ui-component-card--title">QR Code Generator.</h4>
               <p>Ketik teks atau url untuk membuat kode QR.</p>
             </div>
+            </a>
             <div class="ui-component-card ui-layout-column-4">
               <img src="https://res.cloudinary.com/uisual/image/upload/assets/graphics/chat.png" alt="#" loading="lazy" class="ui-component-card--image">
               <h4 class="ui-component-card--title">Lorem ipsum dolor.</h4>
